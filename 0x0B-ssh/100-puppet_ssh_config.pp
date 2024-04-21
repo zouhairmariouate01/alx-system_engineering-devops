fffa0c6221dc884b22bea17f45248f0893f27ed5
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-04-21 04:41:05 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,40 @@
+# ssh_config.pp
+
+include stdlib
+
+file { '/etc/ssh/ssh_config':
+  ensure  => file,
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  content => template('ssh/ssh_config.erb'),
+}
+
+File { '/root/.ssh':
+  ensure => directory,
+  mode   => '0700',
+}
+
+File { '/root/.ssh/config':
+  ensure  => file,
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  content => template('ssh/ssh_config.erb'),
+}
+
+File { '/root/.ssh/school':
+  ensure  => file,
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  source  => 'puppet:///modules/ssh/school',
+}
+
+ssh_config { 'root':
+  ensure => present,
+  user   => 'root',
+  host   => 'your_server_address',
+  identityfile => '/root/.ssh/school',
+  passwordauthentication => false,
+}
