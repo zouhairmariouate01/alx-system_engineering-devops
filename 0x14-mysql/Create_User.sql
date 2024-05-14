188465eb77f787b2c369639a56d9672dfcac08dc
zouhairmariouate01 <zouhairmarioute@gmail.com>
2024-05-14 14:55:48 -0700

    tasks

--- 0x14-mysql/Create_User.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
--- 0x14-mysql/Replica_User.sql
+++
@@ -1,6 +0,0 @@
-GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
-
-CREATE USER 'replica_user'@'%' IDENTIFIED BY 'zouhairmariouate';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'zouhairmariouate';
-FLUSH PRIVILEGES;
